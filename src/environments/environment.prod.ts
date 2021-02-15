43291e00b3aa6b256434a48d32eada25f6748643
mariobeltran99 <37723917+mariobeltran99@users.noreply.github.com>
2021-02-15 16:24:10 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAJa80uTq3BdEd-PgAt_A94GQ5gj2eycOg",
-    authDomain: "vetproyect-29443.firebaseapp.com",
-    databaseURL: "https://vetproyect-29443.firebaseio.com",
-    projectId: "vetproyect-29443",
-    storageBucket: "vetproyect-29443.appspot.com",
-    messagingSenderId: "636881789673",
-    appId: "1:636881789673:web:9cae02afd45ce0d5714678",
-    measurementId: "G-124M611EPT"
+    
   }
 };
